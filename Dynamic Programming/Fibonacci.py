13e56ae7b08e83a65d1fe7c057d50ff981b00f91
Ankit Mishra <43310904+AnkitMishra19007@users.noreply.github.com>
2020-12-04 22:12:51 +0530

    Create Fibonacci.py

---
+++ Dynamic Programming/Fibonacci.py
@@ -0,0 +1,17 @@
+def fib(n,d):
+    //If we already know the value then just return it
+    if n in d:
+        return d[n]
+    else:
+        //compute the new value and add it to dictionary
+        a= fib(n-1,d)+ fib(n-2,d)
+        d[n]= a
+        return a
+
+//Defining an empty dictionary
+d={}
+d[1]=1
+d[2]=1
+
+n= int(input('Which Fibonacci term do you want- '))
+print(fib(n,d))
